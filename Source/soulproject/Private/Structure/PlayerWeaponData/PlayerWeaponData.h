5dc10c622700bf217244ab450dfa52a2dc97b5d4
YEO <logic1yeo@gmail.com>
2024-04-17 17:44:37 +0900

    Weapon Widget ExChange

---
+++ Source/soulproject/Private/Structure/PlayerWeaponData/PlayerWeaponData.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "PlayerWeaponData.generated.h"
+
+/**
+ * 플레이어 무기에 대한 정보를 나타내는 구조체입니다.
+ */
+USTRUCT()
+struct FPlayerWeaponData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	// 무기 이름
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName Name;
+
+	// 무게
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Weight;
+
+	// 공격력
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Atk;
+
+	// 특수 능력
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly)
+
+
+
+};
